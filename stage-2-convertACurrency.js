eb00a1eefc8689d970e8a60dbf362e99849e7420
AstronauticalCodes <hyperskillabhinav@gmail.com>
2024-11-20 00:32:54 +0530

    Modified -> Submittable.

--- stage-2-convertACurrency.js
+++ stage-2-convertACurrency.js
@@ -1,6 +1,9 @@
-// const input = require('sync-input');
+// UDS - Uncomment During Submission
 
-// const currencies = ["JPY", "EUR", "RUB", "USD", "GBP"];
+//region UDS
+const input = require('sync-input');
+//endregion
+
 const currencies = {
     USD: 1,
     JPY: 113.5,
@@ -19,25 +22,35 @@ I can convert USD to these currencies: ${Object.keys(currencies).join(", ")}
 Type the currency you wish to convert: USD`);
 
 const conversion = function(amount, currency) {
-    return (amount * currencies[currency.toUpperCase()]).toFixed(4);
+    return (amount * currencies[currency]).toFixed(4);
 }
 
-// const userTo = input("To: ");
-const userTo = "jpy";
-console.log(`To: > ${userTo}`);
+//region UDS
+const userTo = input("To: ").toUpperCase();
+//endregion
 
-if (!Object.keys(currencies).includes(userTo.toUpperCase())) {
+//region testingValues
+// const userTo = "jpy".toUpperCase();
+// console.log(`To: > ${userTo}`);
+//endregion
+
+if (!Object.keys(currencies).includes(userTo)) {
     console.log("Unknown currency");
 } else {
-    // const userAmount = input("Amount: ");
-    const userAmount = "3";
-    console.log(`Amount: > ${userAmount}`);
+    //region UDS
+    const userAmount = input("Amount: ");
+    //endregion
+
+    //region testingValues
+    // const userAmount = "3";
+    // console.log(`Amount: > ${userAmount}`);
+    //endregion
 
     if (isNaN(parseInt(userAmount))) {
         console.log("The amount has to be a number");
     } else if (parseInt(userAmount) < 1) {
         console.log("The amount cannot be less than 1");
     } else {
-        console.log(`Result: ${userAmount} USD equals ${conversion(parseInt(userAmount), userTo)} ${userTo.toUpperCase()}`);
+        console.log(`Result: ${userAmount} USD equals ${conversion(parseInt(userAmount), userTo)} ${userTo}`);
     }
 }
